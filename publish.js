7b4714b1b3af025356c2e4a1b2ffb2b91d7ea991
AlecTietjens <alec@tietjens.com>
2018-11-29 15:30:22 -0800

    add files

---
+++ publish.js
@@ -0,0 +1,54 @@
+require('dotenv-extended').load();
+
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../samplenodebotwithluis.zip');
+var kuduApi = 'https://samplenodebotwithluis.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = process.env.AzurePublishUsername;
+var password = process.env.AzurePublishPassword;
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('samplenodebotwithluis publish');
+  } else {
+    console.error('failed to publish samplenodebotwithluis', err);
+  }
+});
